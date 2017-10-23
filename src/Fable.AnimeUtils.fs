cf3a3ec50dc9b54ea4778c97eddb448aee0fab7b
francois nicaise <whitetigle@gmail.com>
2017-10-23 11:18:31 +0200

    updated all bindings with current working.

--- src/Fable.AnimeUtils.fs
+++ src/Fable.AnimeUtils.fs
@@ -3,11 +3,26 @@ module Fable.AnimeUtils
 open Fable.Import.Animejs
 open Fable.Core.JsInterop
 
+(*
 let XY (target:'t) xFactor yFactor duration elasticity =
-  jsOptions<anime.AnimeAnimParams> (fun o ->
+  jsOptions<AnimatableProperties> (fun o ->
     o.elasticity <- !!elasticity
     o.duration <- !!duration
     o.targets <- !!target
     o.Item("x") <- xFactor
     o.Item("y") <- yFactor
-  )         
+  )
+
+let SingleParameter (target:'t) parameterName paramaterFactor duration elasticity =
+  jsOptions<anime.AnimeAnimParams> (fun o ->
+    o.elasticity <- !!elasticity
+    o.duration <- !!duration
+    o.targets <- !!target
+    o.Item(parameterName) <- paramaterFactor
+  )
+*)
+
+let GetInstance (options:AnimInput) =
+  let instance = Globals.anime
+  instance.Invoke options
+
